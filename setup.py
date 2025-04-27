b6b75731935ce60065183c23425d5a2d45295060
Trieu Tran <trieut@bu.edu>
2025-04-27 14:15:11 -0400

    Finalized bash installation script

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+package_name = 'p2p_ros_package'
+
 setup(
-    name='p2p_ros_package',
+    name=package_name,
     version='0.1.0',
     packages=find_packages(exclude=['test']),
     install_requires=[
@@ -9,14 +11,21 @@ setup(
         'rclpy',
         'std_msgs'
     ],
-    author='Your Name',
-    author_email='you@example.com',
+    author='Trieu Tran',
+    author_email='trieut@bu.edu',
     description='ROS2 P2P package with CRUD/SQLite support',
     license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'publisher_node = p2p_ros_package.main.publisher:main',
-            'subscriber_node = p2p_ros_package.main.subscriber:main'
+            'publisher = p2p_ros_package.main.publisher:main',
+            'subscriber = p2p_ros_package.main.subscriber:main'
         ],
     },
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['src/p2p_ros_package/launch/p2p_launch.py']),
+    ],
+    zip_safe=True,
 )
+
